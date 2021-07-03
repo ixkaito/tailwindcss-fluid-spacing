ede206c5a240d0780b7afd07323b7f9fcecd43c7
Kite <ixkaito@gmail.com>
2021-07-03 18:57:44 +0900

    Beta 5

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = {
-  plugins: [
-    plugin(({ addUtilities }) => {
-      const dynamicSpacingUtilities = {
-        '.m-vw-4': {
-          margin: '1.25vw',
-        },
-        '.m-vw-8': {
-          margin: '2.5vw',
-        },
-      }
+const dynamicSpacing = plugin(({ addUtilities }) => {
+  const dynamicSpacingUtilities = {
+    '.m-vw-4': {
+      margin: '1.25vw',
+    },
+    '.m-vw-8': {
+      margin: '2.5vw',
+    },
+  }
 
-      addUtilities(dynamicSpacingUtilities)
-    }),
-  ],
+  // addUtilities(dynamicSpacingUtilities)
+},
+{
+  theme: {
+    spacing: {
+      'vw-4': '1.25vw',
+      'vw-8': '2.5vw',
+    },
+  },
+})
+
+exports.spacings = {
+  'vw-4': '1.25vw',
+  'vw-8': '2.5vw',
 }
+
+module.exports = dynamicSpacing
