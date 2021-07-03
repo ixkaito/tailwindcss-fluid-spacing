457cbb68b223c82f8f8724e080be8a626d267a99
Kite <ixkaito@gmail.com>
2021-07-03 22:14:30 +0900

    v0.1.0

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
 const plugin = require('tailwindcss/plugin')
 
-const dynamicSpacing = plugin(({ addUtilities }) => {
-  const dynamicSpacingUtilities = {
-    '.m-vw-4': {
-      margin: '1.25vw',
-    },
-    '.m-vw-8': {
-      margin: '2.5vw',
-    },
-  }
+const rem = (px) => {
+  return (parseInt(px) / 16).toFixed(4).replace(/\.?0+$/, '') + 'rem'
+}
 
-  // addUtilities(dynamicSpacingUtilities)
-},
-{
-  theme: {
-    extend: {
-      spacing: {
-        'vw-4': '1.25vw',
-        'vw-8': '2.5vw',
+const interval = 1.25
+
+const dynamicSpacing = plugin(
+  () => {},
+  {
+    theme: {
+      extend: {
+        spacing: theme => {
+          const screens = theme('screens')
+          const spacing = {}
+
+          for (let i = 1; i <= 16; i++) {
+            const key = `${i * 4}`
+            const vw = interval * i
+
+            spacing[`vw-${key}`] = `${vw}vw`
+
+            for (const screen in screens) {
+              const max = (parseInt(screens[screen], 10) * vw) / 100
+              spacing[`vw-${key}-max-${screen}`] = `min(${vw}vw, ${rem(max)})`
+            }
+          }
+
+          return spacing
+        },
+        margin: theme => {
+          const screens = theme('screens')
+          const margin = {}
+
+          for (let i = 1; i <= 16; i++) {
+            const key = `${i * 4}`
+            const vw = interval * i
+
+            margin[`-vw-${key}`] = `-${vw}vw`
+
+            for (const screen in screens) {
+              const max = (parseInt(screens[screen], 10) * vw) / 100
+              margin[`-vw-${key}-max-${screen}`] = `max(-${vw}vw, -${rem(max)})`
+            }
+          }
+
+          return margin
+        }
       },
     },
-  },
-})
-
-exports.spacings = {
-  'vw-4': '1.25vw',
-  'vw-8': '2.5vw',
-}
+  }
+)
 
 module.exports = dynamicSpacing
