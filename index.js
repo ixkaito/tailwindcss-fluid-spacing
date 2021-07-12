3eeb62343a353feca11eb81def6e61909fa30879
Kite <ixkaito@gmail.com>
2021-07-12 18:56:50 +0900

    Refactoring

--- index.js
+++ index.js
@@ -11,65 +11,69 @@ const dynamicSpacing = plugin.withOptions(
     }
   },
   (options) => {
-    const sizes = [
-      ...(options?.sizes ?? Object.keys(require('tailwindcss/defaultTheme').spacing)),
-      ...(options?.extend?.sizes ?? []),
-    ]
-      .filter((size) => Number(size) !== 0 && !isNaN(size))
-      .map((size) => `${size}`)
+    const propValues = (screens, pn = 'positive') => {
+      const sizes = [
+        ...(options?.sizes ?? Object.keys(require('tailwindcss/defaultTheme').spacing)),
+        ...(options?.extend?.sizes ?? []),
+      ]
+        .filter((size) => Number(size) !== 0 && !isNaN(size))
+        .map((size) => `${size}`)
 
-    const values = sizes.reduce(
-      (acc, cur) => ({
-        ...acc,
-        [cur]: cur * 0.3125,
-      }),
-      {}
-    )
+      const values = sizes.reduce(
+        (acc, cur) => ({
+          ...acc,
+          [cur]: cur * 0.3125,
+        }),
+        {}
+      )
+
+      const prop = {}
+
+      for (const [size, value] of Object.entries(values)) {
+        if (pn === 'positive') {
+          prop[`vw-${size}`] = `${value}vw`
+        }
+
+        for (const [screenName, screenSize] of Object.entries(screens)) {
+          const minmax = (parseFloat(screenSize) * value) / 100
+
+          if (pn === 'positive') {
+            prop[`vw-${size}-min@${screenName}`] = `max(${rem(minmax)}, ${value}vw)`
+            prop[`vw-${size}-max@${screenName}`] = `min(${value}vw, ${rem(minmax)})`
+          } else if (pn === 'negative') {
+            prop[`-vw-${size}-min@${screenName}`] = `min(-${rem(minmax)}, -${value}vw)`
+            prop[`-vw-${size}-max@${screenName}`] = `max(-${value}vw, -${rem(minmax)})`
+          }
+
+          for (const [screenName2, screenSize2] of Object.entries(screens)) {
+            if (parseFloat(screenSize2) <= parseFloat(screenSize)) continue
+            const minmax2 = (parseFloat(screenSize2) * value) / 100
+
+            if (pn === 'positive') {
+              prop[`vw-${size}-min@${screenName}-max@${screenName2}`] = `clamp(
+                ${rem(minmax)}, ${value}vw, ${rem(minmax2)}
+              )`
+            } else if (pn === 'negative') {
+              prop[`-vw-${size}-min@${screenName}-max@${screenName2}`] = `clamp(
+                -${rem(minmax2)}, -${value}vw, -${rem(minmax)}
+              )`
+            }
+          }
+        }
+      }
+
+      return prop
+    }
 
     return {
       theme: {
         extend: {
           spacing: (theme) => {
-            const screens = theme('screens')
-            const spacing = {}
+            return propValues(theme('screens'))
 
-            for (const [size, value] of Object.entries(values)) {
-              spacing[`vw-${size}`] = `${value}vw`
-
-              for (const [screenName, screenSize] of Object.entries(screens)) {
-                const minmax = (parseFloat(screenSize) * value) / 100
-                spacing[`vw-${size}-min@${screenName}`] = `max(${rem(minmax)}, ${value}vw)`
-                spacing[`vw-${size}-max@${screenName}`] = `min(${value}vw, ${rem(minmax)})`
-
-                for (const [screenName2, screenSize2] of Object.entries(screens)) {
-                  if (parseFloat(screenSize2) <= parseFloat(screenSize)) continue
-                  const minmax2 = (parseFloat(screenSize2) * value) / 100
-                  spacing[`vw-${size}-min@${screenName}-max@${screenName2}`] = `clamp(${rem(minmax)}, ${value}vw, ${rem(minmax2)})`
-                }
-              }
-            }
-
-            return spacing
           },
           margin: (theme) => {
-            const screens = theme('screens')
-            const margin = {}
-
-            for (const [size, value] of Object.entries(values)) {
-              for (const [screenName, screenSize] of Object.entries(screens)) {
-                const minmax = (parseFloat(screenSize) * value) / 100
-                margin[`-vw-${size}-min@${screenName}`] = `min(-${rem(minmax)}, -${value}vw)`
-                margin[`-vw-${size}-max@${screenName}`] = `max(-${value}vw, -${rem(minmax)})`
-
-                for (const [screenName2, screenSize2] of Object.entries(screens)) {
-                  if (parseFloat(screenSize2) <= parseFloat(screenSize)) continue
-                  const minmax2 = (parseFloat(screenSize2) * value) / 100
-                  margin[`-vw-${size}-min@${screenName}-max@${screenName2}`] = `clamp(-${rem(minmax2)}, -${value}vw, -${rem(minmax)})`
-                }
-              }
-            }
-
-            return margin
+            return propValues(theme('screens'), 'negative')
           },
         },
       },
